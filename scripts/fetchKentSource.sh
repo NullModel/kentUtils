a6ec02857b4df44db9e34c51c295232d19e05e5a
Null Model <nullmodel@gmail.com>
2013-03-29 11:23:10 -0700

    adding samtabix fetch

--- scripts/fetchKentSource.sh
+++ scripts/fetchKentSource.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git clone http://genome-source.cse.ucsc.edu/samtabix.git samtabix
 git archive --remote=git://genome-source.cse.ucsc.edu/kent.git \
 --prefix=kent/ HEAD \
 src/product \
